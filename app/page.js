8dd75eb151492e952726ad65ec29cdc63be83e42
2213saabji <2213saabji@gmail.com>
2024-02-16 12:40:31 +0530

    commit for checking somehting

--- app/page.js
+++ app/page.js
@@ -12,6 +12,7 @@ export default function Home() {
   const router=useRouter();
   const { theme, settheme, logintoken, setlogintoken, loader, setloader, blackscreen2, setblackscreen2, themecheck } = allContext();
   const [data, setdata] = useState();
+  const [toggle,settoggle]=useState(false);
   const fetchdata = useMemo(async () => {
     try {
       const response = await (await fetch(`${baseurl}/quora/post?limit=500`,
@@ -23,18 +24,64 @@ export default function Home() {
         }
       )).json();
       setdata(response.data)
+      console.log(response);
     } catch (error) {
       alert(error);
     }
-  }, [])
+  }, [toggle])
   useEffect(() => {
     fetchdata;
   }, [])
+  
+  const likefun = async (val) => {
+    try {
+      const response = await (await fetch(`${baseurl}/quora/like/${val}`,
+        {
+          method: "POST",
+          headers: {
+            projectID: "7zwdmzusuw4h",
+            Authorization: `Bearer ${JSON.parse(localStorage.getItem("token"))}`
+          }
+        }
+      )).json();
+      settoggle(!toggle)
 
+    } catch (error) {
+      alert(error);
+    }
+  }
+  const dislikefun = async (val) => {
+    try {
+      const response = await (await fetch(`${baseurl}/quora/dislike/${val}`,
+        {
+          method: "POST",
+          headers: {
+            projectID: "7zwdmzusuw4h",
+            Authorization: `Bearer ${JSON.parse(localStorage.getItem("token"))}`
+          }
+        }
+      )).json();
+      settoggle(!toggle)
+      console.log(response)
+    } catch (error) {
+      alert(error);
+    }
+  }
 
   function routetouserpage(userid){
     router.push(`/profile/${userid}`)
     console.log(userid)
+  }
+
+  function clicklike(like,id){
+    if(like==false){
+      likefun(id);
+    }
+  }
+  function clickdislike(dislike,id){
+    if(dislike==false){
+      dislikefun(id);
+    }
   }
   
   return <div id="App" className="mainflex">
@@ -78,7 +125,10 @@ export default function Home() {
             </div>
             <img className={`w100per`} style={{height:"100%"}} src={item.images[0]} alt="image"/>
             <div className={`flex pl20 pr20 pb5 mt5`}>
-              <div className={`brdr-r50 csrpntr flex brdr1 ${themecheck("brdrlightgray", "brdrllgray")} `}><p className={`flexa bghvr brdrr1 upvotebtn pt5 pl10 pr10 pb5 fnt13 ${themecheck("brdrlightgray", "brdrllgray")}`}>{upvoteicon}&nbsp;Upvote . &nbsp;{item.likeCount}</p><p className={`pl10 pr10  bghvr downvotebtn flexja`}>{downvoteicon}</p></div>
+              <div className={`brdr-r50 csrpntr flex brdr1 ${themecheck("brdrlightgray", "brdrllgray")} `}>
+                <p className={`flexa bghvr brdrr1 upvotebtn pt5 pl10 pr10 pb5 fnt13 ${themecheck("brdrlightgray", "brdrllgray")}`} onClick={()=>{clicklike(item.isLiked,item._id)}}>{upvoteicon}&nbsp;Upvote . &nbsp;{item.likeCount}</p>
+                <p className={`pl10 pr10  bghvr downvotebtn flexja`} onClick={()=>{clickdislike(item.isDisliked,item._id)}}>{downvoteicon}</p>
+                </div>
               <div className={`bghvr csrpntr brdr-r12 flexja ml20`}>{roundmessageicon}&nbsp;{item.commentCount}</div>
               </div>
           </div>
